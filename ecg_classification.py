91f5050b1953421517057b7a199fe94d9a4180ad
sophia spaner <vpq5kd@virginia.edu>
2025-12-21 22:17:46 -0500

    used sklearn MultiLabelBinarizer to encode the string type classes to binary labels

--- ecg_classification.py
+++ ecg_classification.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import wfdb
 import ast
+import torch
 from sklearn import svm
-
+from sklearn.preprocessing import MultiLabelBinarizer
 
 #processing code based on "example_physionet.py" from the dataset
 
@@ -24,7 +25,6 @@ Y.scp_codes = Y.scp_codes.apply(lambda x: ast.literal_eval(x))
 try:
     print("attempting to load presaved np array")
     X = np.load("lowresolution_ecg_nparray.npy")
-    print(X)
 except OSError:
     print("np array load failed or does not exist, proceeding with hard load")
     X = load_raw_data(Y, sampling_rate, path)
@@ -46,13 +46,23 @@ Y['diagnostic_superclass'] = Y.scp_codes.apply(aggregate_diagnostic)
 
 test_fold = 10
 
-X_train = X[np.where(Y.strat_fold != test_fold)]
+X_train = torch.from_numpy(X[np.where(Y.strat_fold != test_fold)])
 Y_train = Y[(Y.strat_fold != test_fold)].diagnostic_superclass
 
-X_test = X[np.where(Y.strat_fold == test_fold)]
+
+X_test = torch.from_numpy(X[np.where(Y.strat_fold == test_fold)])
 Y_test = Y[Y.strat_fold == test_fold].diagnostic_superclass
 
-clf = svm.SVC()
-clf.fit(X_train, Y_train)
+
+#encode the data for pytorch to accept
+Y_train = Y_train.to_numpy()
+Y_test = Y_test.to_numpy()
+
+mlb = MultiLabelBinarizer()
+
+Y_train_encoded = torch.from_numpy(mlb.fit_transform(Y_train))
+Y_test_encoded = torch.from_numpy(mlb.transform(Y_test))
+
+
 
 
