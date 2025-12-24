7fb14ba3b9395778a8519df8a3cf20246453a33d
sophia spaner <vpq5kd@virginia.edu>
2025-12-24 11:03:03 -0500

    added a results script to isolate the python file where I display results.

---
+++ ecg_classification_nn_results.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+from ecg_classification_nn_testing import all_predictions, all_labels
+
+
--- ecg_classification_nn_testing.py
+++ ecg_classification_nn_testing.py
@@ -14,6 +14,7 @@ inputs, labels = next(data)
 clf.load_state_dict(torch.load(PATH))
 
 #test the test data with the model
+print("testing the model")
 clf.eval()
 all_predictions = []
 all_labels = []
@@ -29,23 +30,14 @@ with torch.no_grad():
         all_labels.append(labels)
 all_predictions = torch.cat(all_predictions, dim=0)
 all_labels = torch.cat(all_labels, dim = 0)
+print("testing complete")
 
 #analyze results
+
 all_predictions_np = all_predictions.numpy()
 all_labels_np = all_labels.numpy()
 
 all_predictions_np_transformed = mlb.inverse_transform(all_predictions_np)
 all_labels_np_transformed = mlb.inverse_transform(all_labels_np)
 
-mi_tp = 0
-mi_fn = 0
-for i in range(len(all_labels_np_transformed)):
-    pred = all_predictions_np_transformed[i]
-    true = all_labels_np_transformed[i]
-    
-    if (('MI' in pred) and  ('MI' in true)):
-        mi_tp += 1
-    if (('MI' not in pred) and ('MI' in true)):
-        mi_fn += 1
 
-print(f"true positive MI: {mi_tp}, false negative MI: {mi_fn}")
