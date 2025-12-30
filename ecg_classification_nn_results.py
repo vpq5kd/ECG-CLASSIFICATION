7671a1b63c527d6426cc11925056e74111294d20
sophia spaner <vpq5kd@virginia.edu>
2025-12-30 12:45:02 -0500

    working on confusion matrix plot

--- ecg_classification_nn_results.py
+++ ecg_classification_nn_results.py
@@ -10,9 +10,8 @@ from ecg_classification_nn_training import mlb
 all_predictions = all_predictions.numpy()
 all_labels = all_labels.numpy()
 
-print(mlb.classes_)
+classes = mlb.classes_
 mcm = multilabel_confusion_matrix(all_labels, all_predictions)
-print(mcm)
 
 num_classes = len(mlb.classes_)
 TP = sum(mcm[k,1,1] for k in range(num_classes))
@@ -20,6 +19,21 @@ TN = sum(mcm[k,0,0] for k in range(num_classes))
 FN = sum(mcm[k,1,0] for k in range(num_classes))
 FP = sum(mcm[k,0,1] for k in range(num_classes))
 
+fig, ax = plt.subplots(1,5)
+vmax = mcm.max()
+for i, cm in enumerate(mcm):
+    ylabels = ["",""]
+    xlabels = ["",""]
+    if (i==0):
+        ylables = ["Actually Negative", "Actually Positive"]
+        xlabels = ["Predicted Negative", "Predicted Positive"]
+
+    sns.heatmap(cm, ax=ax[i], vmin=0, vmax=vmax,cbar=False, square=True,  annot=True, fmt="d", cmap="Blues", xticklabels=xlabels, yticklabels=ylabels)
+    ax[i].set_title(f"{classes[i]}")
+
+fig.colorbar(ax[0].collections[0], ax=ax, location ="right")
+plt.tight_layout()
+plt.show()
 print(f"global accuracy: {100*(TP+TN)/(TP+TN+FN+FP)}%")
 
 
